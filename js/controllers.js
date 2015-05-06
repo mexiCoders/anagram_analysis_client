fea8814d64e5c0e3a5a37d3b1bf89c08ef816167
Daniel Martinez <roanhawkeye@gmail.com>
2015-05-06 13:12:22 -0500

    Add form to consume REST service

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,21 @@
 (function () {
     angular.module('anagramController',[])
-        .controller('indexController', ['$scope', function ($scope) {
-            $scope.message = "Hello";
+        .controller('indexController', ['$scope', '$http', function ($scope, $http) {
+
+            $scope.isAnagram = function () {
+                var word1 = $scope.word1,
+                word2 = $scope.word2;
+
+                var apiUrl = 'http://localhost:8080/anagramfinder?firstword='+word1+'&secondword='+word2;
+                console.log(apiUrl);
+                $http({
+                    method: 'POST',
+                    url: apiUrl
+                }).success(function (data) {
+                    $scope.anagram = data;
+                }).error( function () {
+                    $scope.errorMessage = 'Error trying to reach server';
+                });
+            };
         }]);
 })();
